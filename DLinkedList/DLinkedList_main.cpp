1be66a7fb2c4ad2837fd83aa0a29ea4718283c1f
ryanshim <ryan.shim@gmail.com>
2017-03-16 14:30:03 -0700

    added main function file

--- DLinkedList/DLinkedList_main.cpp
+++ DLinkedList/DLinkedList_main.cpp
@@ -1,11 +1,21 @@
 // Main function for Doubly Linked List implementation
 //
+#include "DLinkedList.h"
 #include <iostream>
 #include <cassert>
-#include "DLinkedList.h"
 
 using namespace std;
 
 int main() {
+    DLinkedList<int> intNums;
+    int testNum = 0;
+
+    // add elements
+    for (int i = 100; i < 600; i += 100) {
+        intNums.addBack(i);
+    }
+    testNum = intNums.front(); 
+    assert(testNum == 100);
+
     return 0;
 }
