d7430debc94f11bbf3eb8cd7bb2f322769a3d4d3
ryanshim <ryan.shim@gmail.com>
2017-03-18 18:36:37 -0700

    Added unit tests to main funtion

--- DLinkedList/DLinkedList_main.cpp
+++ DLinkedList/DLinkedList_main.cpp
@@ -8,14 +8,38 @@ using namespace std;
 
 int main() {
     DLinkedList<int> intNums;
-    int testNum = 0;
+    int testNumFront = 0;
+    int testNumBack = 0;
 
     // add elements
     for (int i = 100; i < 600; i += 100) {
         intNums.addBack(i);
     }
-    testNum = intNums.front(); 
-    assert(testNum == 100);
+    testNumFront = intNums.front(); 
+    testNumBack = intNums.back();
+    cout << "Current front element: " << testNumFront << endl;
+    cout << "Current back element: " << testNumBack << endl;
+    assert(testNumFront == 100);
+    assert(testNumBack == 500);
+
+    intNums.removeFront();  // remove front element
+    intNums.removeBack();   // remove back element
+
+    testNumFront = intNums.front(); 
+    testNumBack = intNums.back();
+    cout << "Current front element: " << testNumFront << endl;
+    cout << "Current back element: " << testNumBack << endl;
+    assert(testNumFront == 200);
+    assert(testNumBack == 400);
+
+    intNums.removeFront();  // remove front element
+
+    testNumFront = intNums.front(); 
+    testNumBack = intNums.back();
+    cout << "Current front element: " << testNumFront << endl;
+    cout << "Current back element: " << testNumBack << endl;
+    assert(testNumFront == 300);
+    assert(testNumBack == 400);
 
     return 0;
 }
