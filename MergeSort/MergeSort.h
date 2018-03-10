36a2ee1af8a0665dda3ffe9f1c90d2a864b7850f
ryanshim <ryan.shim@gmail.com>
2018-03-10 02:44:55 -0800

    merge sort working

--- MergeSort/MergeSort.h
+++ MergeSort/MergeSort.h
@@ -1,8 +1,10 @@
 // Merge sort header file
 #pragma once
-#include <iostream> // for editor, remove later
-#include <vector>   // for editor, remove later
-#include <stdexcept>
+#include <iostream>
+#include <vector>
+
+template <typename E>
+std::vector<E> merge(std::vector<E> L, std::vector<E> R);   // forward declaration
 
 template <typename E>
 std::vector<E> mergeSort(std::vector<E> a) {
@@ -10,13 +12,17 @@ std::vector<E> mergeSort(std::vector<E> a) {
         return a;
     }
     else {
+        unsigned half = a.size() / 2;
+
         // L, R = <DIVIDE V INTO TWO INSTANCES OF ROUGHLY EQUAL SIZE>
-        std::vector<E> L, R;
-        for (unsigned i = 0; i < a.size() / 2; i++) {
+        std::vector<E> L;
+        std::vector<E> R;
+        for (unsigned i = 0; i < half; i++) {
             L.push_back(a[i]);
         }
-        for (unsigned j = a.size() / 2; j < a.size(); j++) {
-            R.push_back(a[j]);
+
+        for (unsigned i = half; i < a.size(); i++) {
+            R.push_back(a[i]);
         }
 
         std::vector<E> sortedL, sortedR, sortedV;
@@ -28,10 +34,39 @@ std::vector<E> mergeSort(std::vector<E> a) {
         sortedR = mergeSort(R);
 
         // sorted_V = <COMBINE sorted_L WITH sorted_R>
-        
+        sortedV = merge(sortedL, sortedR);
+
         // return sorted_V
         return sortedV;
     }
+}
+
+template <typename E>
+std::vector<E> merge(std::vector<E> L, std::vector<E> R) {
+    std::vector<E> v;
+    unsigned li = 0;
+    unsigned ri = 0;
+
+    while (li < L.size() && ri < R.size()) {
+        if (L[li] <= R[ri]) {
+            v.push_back(L[li]);
+            li++;
+        }
+        else {
+            v.push_back(R[ri]);
+            ri++;
+        }
+    }
+
+    // only one of these loops will execute
+    for (; li < L.size(); li++) {
+        v.push_back(L[li]);
+    }
+    for (; ri < R.size(); ri++) {
+        v.push_back(R[ri]);
+    }
+    
+    return v;
 }
 
 
--- MergeSort/MergeSort_main.cpp
+++ MergeSort/MergeSort_main.cpp
@@ -1,10 +1,8 @@
 // Main for testing mergesort implementation
-#include <iostream>
-#include <vector>
-#include <ctime>
 #include "MergeSort.h"
+#include <ctime>
 
-#define NUM_ELTS 10
+#define NUM_ELTS 20 
 
 int main() {
 
@@ -12,17 +10,22 @@ int main() {
 
     std::vector<int> xVect; // create an empty vector
     std::vector<int> sortedVect;
+    int value;
 
     // populate vector with random numbers b/t 1 to 10
+    std::cout << "Unordered Vector:" << std::endl;;
     for (int i = 0; i < NUM_ELTS; i++) {
-        xVect.push_back(i);
-        //xVect.push_back(rand() % 10 + 1);
+        value = rand() % 10 + 1;
+        std::cout << value << " ";
+        xVect.push_back(value);
     }
+    std::cout << std::endl << std::endl;;
 
     sortedVect = mergeSort(xVect);
 
-    for (int j = 0; j < NUM_ELTS; j++) {
-        std::cout << sortedVect[j] << " ";
+    std::cout << "Ordered Vector:" << std::endl;
+    for (unsigned i = 0; i < sortedVect.size(); i++) {
+        std::cout << sortedVect[i] << " ";
     }
     std::cout << std::endl;
 
